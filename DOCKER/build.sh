cc5d3cf3be4438bdd39ab452fdffad11fffb04f9
Marko <marko.eremija@amres.ac.rs>
2017-07-11 15:38:13 +0200

    Create custom Docker image locally
    This script creates Docker image based on the Dockerfile, but does not push it to Docker Hub. Backward compatibility maintained with the second Docker build command.

--- DOCKER/build.sh
+++ DOCKER/build.sh
@@ -8,5 +8,11 @@ else
     TAG=":latest"
 fi
 
-docker build -t stretch-shib-idp-test01$TAG .
+version=`docker version --format '{{.Client.Version}}'`
+current_year=`date +"%y"`
 
+if [[ "$version" == *"$current_year"* ]]; then
+  docker image build -t stretch-shib-idp-test01$TAG .
+else
+  # keep backward compatibility with older versions of Docker Engine
+  docker build -t stretch-shib-idp-test01$TAG .
