b68848bc840d6a37b2b11b9e375301e2edeee1be
Marko <marko.eremija@amres.ac.rs>
2017-07-11 15:25:58 +0200

    Run a Docker container
    This script is used to run a Docker container, with both "legacy" command and the new format of command. If Docker CE is not installed, a message is printed to notify the user.

--- DOCKER/run.sh
+++ DOCKER/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script will work only if Docker CE is installed.
+# Please use install.sh script to install all the dependencies
+# needed to run a container.
+
 mkdir -p /var/tmp/docker-logs-jetty \
          /var/tmp/docker-logs-shibboleth
 
@@ -7,14 +11,39 @@ if [[ $1 ]]; then
     TAG=":$1"
 fi
 
-docker run \
-    -it \
-    --rm \
-    --detach \
-    --name stretch-shib-idp-test01 \
-    --hostname stretch-shib-idp-test01 \
-    -p 8080:8080 -p 8443:8443 \
-    -v /var/tmp/docker-logs-jetty:/opt/jetty/logs \
-    -v /var/tmp/docker-logs-shibboleth:/opt/shibboleth-idp/logs \
-    stretch-shib-idp-test01$TAG
+# Verify that Docker CE is installed
+is_installed=$( which docker )
 
+if [[ "$?" -eq 0  ]]; then
+
+  # Docker major versions now have year as thier version with month
+  # extension, e.g. version 17.06 means it's created in June 2017.
+  # Next two lines are used to compare the short year number and Docker version.
+  # If they match, use the extended Docker command for running containers.
+
+  version=`docker version --format '{{.Client.Version}}'`
+  current_year=`date +"%y"`
+
+  if [[ "$version" == *"$current_year"* ]]; then
+      docker container run \
+      --detach \
+      --name stretch-shib-idp-test01 \
+      --hostname stretch-shib-idp-test01 \
+      -p 8080:8080 -p 8443:8443 \
+      -v /var/tmp/docker-logs-jetty:/opt/jetty/logs \
+      -v /var/tmp/docker-logs-shibboleth:/opt/shibboleth-idp/logs \
+      stretch-shib-idp-test01$TAG
+    else
+      docker run \
+      --detach \
+      --name stretch-shib-idp-test01 \
+      --hostname stretch-shib-idp-test01 \
+      -p 8080:8080 -p 8443:8443 \
+      -v /var/tmp/docker-logs-jetty:/opt/jetty/logs \
+      -v /var/tmp/docker-logs-shibboleth:/opt/shibboleth-idp/logs \
+      stretch-shib-idp-test01$TAG
+    fi
+else
+  printf "Docker CE is not installed.\n"
+  printf "Please use install.sh script to install Docker CE!\n"
+fi
