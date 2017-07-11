3986f360724305136d3327446f453604b0a464dd
Marko <marko.eremija@amres.ac.rs>
2017-07-11 15:41:15 +0200

    Stop and remove containers
    This script gathers the information about how many containers are running, stops all of them and then removes the stopped containers.

--- DOCKER/stop.sh
+++ DOCKER/stop.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-docker stop stretch-shib-idp-test01
+# Get container name
+container_name=(`docker container ps --format "{{.Names}}"`)
+length=${#container_name[@]}
 
+# Stop the container(s) with the specified name(s) and remove it.
+# It helps to keep the Docker environment clean.
+# Use for testing purposes.
+
+if [[ $length -eq "1" ]]; then
+
+  echo "Container with name $container_name will be stopped and removed `date +%Y.%m.%d-%H:%M:%S`" >> /tmp/docker.log
+  docker container stop $container_name && docker container rm $container_name
+
+elif [[ $length -gt "1" ]]; then
+  for ((i=0; i<length; i++))
+  do
+    echo "Container with name ${container_name[i]} will be stopped and removed `date +%Y.%m.%d-%H:%M:%S`" >> /tmp/docker.log
+    docker container stop ${container_name[i]} && docker container rm ${container_name[i]}
+  done
+
+else
+  echo "There are not any running containers at the moment! Run one by using the run.sh script in this directory!"
+fi
