ade110b7acacdefb7fae242f7cad46dc0b5be156
Jan Oppolzer <jan.oppolzer@cesnet.cz>
2017-09-22 13:51:36 +0200

    Improve building process
    Jetty variables are now set in an external file and not hardcoded in the
    Dockerfile.

--- DOCKER/build.sh
+++ DOCKER/build.sh
@@ -11,5 +11,10 @@ fi
 source ./buildenv.conf
 docker image build \
     --build-arg JAVA_HOME=$JAVA_HOME \
+    --build-arg JETTY_VERSION=$JETTY_VERSION \
+    --build-arg JETTY_KEY=$JETTY_KEY \
+    --build-arg PASSWORD_CERT_KEY=$PASSWORD_CERT_KEY \
+    --build-arg PASSWORD_PKCS12=$PASSWORD_PKCS12 \
+    --build-arg PASSWORD_KEYSTORE=$PASSWORD_KEYSTORE \
     -t stretch-shib-idp-test01$TAG .
 
