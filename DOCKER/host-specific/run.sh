df8633b8778438e93361122d3376c1749ec32c79
Jan Oppolzer <jan@oppolzer.cz>
2018-03-20 15:29:13 +0100

    Allow to specify ports when calling run.sh

--- DOCKER/host-specific/run.sh
+++ DOCKER/host-specific/run.sh
@@ -1,11 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+PORT_HTTP=80
+PORT_HTTPS=443
 
 if [[ -z $1 ]]; then
     echo "Missing hostname of the target machine."
     exit
 fi
 
+if [[ -z $2 ]]; then
+    echo "No port for HTTP specified, using 80."
+else
+    PORT_HTTP=$2
+    echo "Using port ${PORT_HTTP} for HTTP."
+fi
+
+if [[ -z $3 ]]; then
+    echo "No port for HTTPS specified, using 443."
+else
+    PORT_HTTPS=$3
+    echo "Using port ${PORT_HTTPS} for HTTPS."
+fi
+
 source ./hosts/$1/buildenv.conf
+
+echo "Using Docker Volume vol-${SHIBBOLETH_HOSTNAME}..."
 
 mkdir -p /var/tmp/docker-logs-jetty \
          /var/tmp/docker-logs-shibboleth
@@ -16,7 +35,7 @@ docker container run \
     --detach \
     --name idp-$SHIBBOLETH_HOSTNAME \
     --hostname idp-$SHIBBOLETH_HOSTNAME \
-    -p 80:8080 -p 443:8443 \
+    -p $PORT_HTTP:8080 -p $PORT_HTTPS:8443 \
     --mount source=vol-$SHIBBOLETH_HOSTNAME,destination=/opt \
     -v /var/tmp/docker-logs-jetty:/opt/jetty/logs \
     -v /var/tmp/docker-logs/shibboleth:/opt/shibboleth-idp/logs \
